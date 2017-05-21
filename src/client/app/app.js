5e7f074192d4e4755de0e7b1830e79e4ff890f58
ZihanZhang <zhang.ziha@husky.neu.edu>
2017-05-20 23:51:49 -0400

    react third part library  indexing test

---
+++ src/client/app/app.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div style={{height: '100%'}}>
+                Map me!
+            </div>
+        );
+    }
+}
--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,63 +1,16 @@
 import React from 'react';
 import {render} from 'react-dom';
+import Appl from './App'
 
 class App extends React.Component {
-
-
   
-  render () {
-    return (<div></div>);
+  render() {
+    return (
+      <div>
+        <Appl />
+      </div>
+    )
   }
 }
 
-function initMap() {
-
-  var map;
-
-  // Constructor creates a new map - only center and zoom are required.
-  map = new google.maps.Map(document.getElementById('app'), {
-    center: {lat: 42.346779, lng: -71.093696},
-    zoom: 15
-  });
-
-  var marker = new google.maps.Marker({
-    position: {lat: 42.346779, lng: -71.093696},
-    map: map,
-    title: 'First Marker!'
-  });
-
-  var neighbors = [
-    {
-      name: 'Wang Dudu',
-      location: '1404 Commonwealth'
-    }
-  ]
-
-  var geocoder = new google.maps.Geocoder();
-
-  var bounds = new google.maps.LatLngBounds();
-  bounds.extend(map.getCenter());
-  map.fitBounds(bounds);
-
-  geocoder.geocode(
-      { address: neighbors[0].location,
-        componentRestrictions: {locality: 'Boston'}
-      }, function(results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-          var marker = new google.maps.Marker({
-            position: results[0].geometry.location,
-            map: map,
-            title: 'First Marker!'
-          });
-
-          bounds.extend(results[0].geometry.location);
-          map.fitBounds(bounds);
-
-        } else {
-          window.alert('We could not find that location - try entering a more' +
-              ' specific place.');
-        }
-      });
-}
-
-render(<App/>, document.getElementById('app'));
+render(<App />, document.getElementById('app'));
