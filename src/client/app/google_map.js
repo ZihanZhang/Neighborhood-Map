d48af46375e4f7ac16bfb827a1d384d875ed5884
ZihanZhang <zhang.ziha@husky.neu.edu>
2017-05-23 18:12:20 -0400

    transfer from html to react version done

--- src/client/app/google_map.js
+++ src/client/app/google_map.js
@@ -1,15 +1,57 @@
 import React, {Component} from 'react';
 
 export default class extends Component {
+
     shouldComponentUpdata() {
         return false;
     }
 
     componentDidMount() {
-        this.map = new google.maps.Map( this.refs.map, {
+        var map;
+        map = new google.maps.Map( this.refs.map, {
             center: {lat: this.props.lat, lng: this.props.lng},
             zoom: 15
-        })
+        });
+
+        var marker = new google.maps.Marker({
+          position: {lat: 42.346779, lng: -71.093696},
+          map: map,
+          title: 'First Marker!'
+        });
+
+        var neighbors = [
+          {
+            name: 'Wang Dudu',
+            location: '1404 Commonwealth'
+          }
+        ]
+
+        var geocoder = new google.maps.Geocoder();
+
+        var bounds = new google.maps.LatLngBounds();
+        bounds.extend(map.getCenter());
+        map.fitBounds(bounds);
+
+        geocoder.geocode(
+            { address: neighbors[0].location,
+              componentRestrictions: {locality: 'Boston'}
+            }, function(results, status) {
+              if (status == google.maps.GeocoderStatus.OK) {
+                var marker = new google.maps.Marker({
+                  position: results[0].geometry.location,
+                  map: map,
+                  title: 'First Marker!'
+                });
+
+                bounds.extend(results[0].geometry.location);
+                map.fitBounds(bounds);
+
+              } else {
+                window.alert('We could not find that location - try entering a more' +
+                    ' specific place.');
+              }
+            }
+        );
     }
 
   render() {
